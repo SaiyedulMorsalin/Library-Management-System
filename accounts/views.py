125fd27a5a203e4e8019a8411c2d362ad08b0be4
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-08-11 20:47:03 +0600

    added some change

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Transaction
+
 
 # Create your views here.
+class TransactionCreateMixin(LoginRequiredMixin, CreateView):
+    model = Transaction
+    template_name = ""
+    success_url = ""
+
+
+class DepositMoney(TransactionCreateMixin):
+    pass
