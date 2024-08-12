eccf8ea9170e44a2b6cdc7f3377e97c93a4d4ac0
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-08-12 23:26:00 +0600

    added some changes

--- core/static/src/script.js
+++ core/static/src/script.js
@@ -1,10 +1,9 @@
-const onImageClickDetails = () => {
-    document.getElementById('img').addEventListener('click', () => {
-        window.location.href = 'https://example.com';
-    })
+const clickTab = (tab) => {
+    // Remove 'tab-active' class from all tabs
+    document.querySelectorAll('.tab').forEach(element => {
+        element.classList.remove('tab-active');
+    });
 
-};
-
-
-
-
+    // Add 'tab-active' class to the clicked tab
+    tab.classList.add('tab-active');
+}
