e46b7ad3c67d0c863b3a9bb3791557e961818838
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-08-13 12:19:51 +0600

    changes users views

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from .views import UserProfileView, UserLoginView
 
 urlpatterns = [
     # "users/profile/logout",
+    path("profile/", UserProfileView.as_view(), name="user_profile"),
+    path("user/login/", UserLoginView.as_view(), name="user_login"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,28 +1,42 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
-
+from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import TemplateView, UpdateView, FormView
 
 
 # Create your views here.
-class UserLoginView(LoginView):
-
-    form_class = AuthenticationForm
+class UserRegisterView(FormView):
+    template_name = "register.html"
+    form_class = ".forms/register_form"
+    success_url = reverse_lazy("register")
 
     def form_valid(self, form):
-        user = form.user
+        user = form.save()
         login(self.request, user)
         return super().form_valid(form)
 
 
-class UserLogoutView:
-    pass
+class UserLoginView(LoginView):
+    template_name = "login.html"
+    form_class = AuthenticationForm
+
+    def get_success_url(self):
+        return reverse_lazy("home_page")
 
 
-class UserProfileView:
-    pass
+class UserLogoutView(LogoutView):
+    def get_success_ulr(self):
+        if self.request.user.is_authenticated:
+            logout(self.request)
+
+        return reverse_lazy("home_page")
 
 
-class UserEditProfile:
+class UserProfileView(TemplateView):
+    template_name = "user_profile.html"
+
+
+class EditUserProfile(UpdateView):
     pass
