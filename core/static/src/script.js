c216d28926f20b14913d13a264b8f9fc5e81b381
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-08-13 12:47:13 +0600

    added some changes

--- core/static/src/script.js
+++
@@ -1,9 +0,0 @@
-const clickTab = (tab) => {
-    // Remove 'tab-active' class from all tabs
-    document.querySelectorAll('.tab').forEach(element => {
-        element.classList.remove('tab-active');
-    });
-
-    // Add 'tab-active' class to the clicked tab
-    tab.classList.add('tab-active');
-}
