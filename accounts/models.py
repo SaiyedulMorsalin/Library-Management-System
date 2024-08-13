8c7a73cf8abf15f6a71b65bdd6ab54b16abf5371
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-08-14 02:19:23 +0600

    added some change on register html

--- accounts/models.py
+++ accounts/models.py
@@ -4,14 +4,14 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class UserAccount(models.Model):
-    user = models.OneToOneField(User, related_name="accounts", on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name="account", on_delete=models.CASCADE)
     account_create_date = models.DateField(auto_now_add=True)
     balance = models.DecimalField(max_digits=12, decimal_places=2, default=0)
 
 
 class Transaction(models.Model):
     account = models.ForeignKey(
-        UserAccount, related_name="transactions", on_delete=models.CASCADE
+        UserAccount, related_name="transaction", on_delete=models.CASCADE
     )
     amount = models.DecimalField(
         max_digits=12,
--- users/forms.py
+++ users/forms.py
@@ -12,3 +12,13 @@ class UserRegisterForm(UserCreationForm):
             "last_name",
             "email",
         ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update(
+                {
+                    "class": ("input input-bordered w-full max-w-xs"),
+                    "placeholder": ("Type here"),
+                }
+            )
