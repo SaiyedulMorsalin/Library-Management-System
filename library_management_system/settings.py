3b69040b8cedb448a2488ff90335c1cbe790d8de
Md Saiyedul Morsalin <132201648+SaiyedulMorsalin@users.noreply.github.com>
2024-08-18 08:08:51 +0600

    added all done

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -4,12 +4,28 @@ import os
 import environ
 import dj_database_url
 
+env = environ.Env()
+environ.Env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # EmailSetup
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL")
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
 
+
+# SECURITY WARNING:
+SECRET_KEY = env("SECRET_KEY")
+
+DEBUG = env("DEBUG")
+
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = [""]
 # Media path
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
